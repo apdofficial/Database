61286fc8808ee5d25eb4ada730a050486ddcdea4
Andrej Pištek <450966@student.saxion.nl>
2019-01-11 09:21:24 +0100

    Update of sql

--- WeatherStation.sql
+++ WeatherStation.sql
@@ -11,20 +11,20 @@ USE Weatherstation;
 SET time_zone = '+01:00';
 
 CREATE TABLE Device(
-    HardwareID VARCHAR(40) not null,
-    DeviceName VARCHAR(40) not null,
-    Location VARCHAR(40) not null,
-    PRIMARY KEY (HardwareID)
+		HardwareID VARCHAR(40) not null,
+		DeviceName VARCHAR(40) not null,
+		Location VARCHAR(40) not null,
+		PRIMARY KEY (HardwareID)
     );
 
 CREATE TABLE Devicedata(
-	HardwareID VARCHAR(40) not null,
-    DataNr int default 0,
-    Temperature double(4,1),
-    Luminosity INT not null,
-    Humidity INT not null,
-    Pressure INT not null ,
-    Time_Interval Datetime,
-    PRIMARY KEY (HardwareID, DataNr ),
-    FOREIGN KEY (HardwareID) REFERENCES Device(HardwareID)
+		HardwareID VARCHAR(40) not null,
+		DataNr int default 0,
+		Temperature double(4,1),
+		Luminosity INT not null,
+		Humidity INT not null,
+		Pressure INT not null ,
+		Time_Interval Datetime,
+		PRIMARY KEY (HardwareID, DataNr ),
+		FOREIGN KEY (HardwareID) REFERENCES Device(HardwareID)
     );
